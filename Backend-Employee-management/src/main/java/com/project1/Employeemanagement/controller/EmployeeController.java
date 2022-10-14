f068e2fdf70db5b1979e4c7b4b4629bc452db336
sandeshghimire2020 <40518788+sandeshghimire2020@users.noreply.github.com>
2022-10-13 20:56:46 -0500

    update-backend

--- Backend-Employee-management/src/main/java/com/project1/Employeemanagement/controller/EmployeeController.java
+++ Backend-Employee-management/src/main/java/com/project1/Employeemanagement/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package com.project1.Employeemanagement.controller;
 
+import com.project1.Employeemanagement.exception.ResourseNotFoundException;
 import com.project1.Employeemanagement.model.Employee;
 import com.project1.Employeemanagement.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,8 +27,27 @@ public class EmployeeController {
 
     @PostMapping("/employees")
     public Employee createEmployee(@RequestBody Employee employee){
-
         return employeeRepository.save(employee);
+    }
+
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id){
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourseNotFoundException("Employee doesn't exist"));
+        return ResponseEntity.ok(employee);
+
+    }
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee>  updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails){
+
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourseNotFoundException("Employee doesn't exist"));
+
+        employee.setFirstName(employeeDetails.getFirstName());
+        employee.setLastName(employeeDetails.getLastName());
+        employee.setEmailId(employeeDetails.getEmailId());
+        Employee updatedEmployee =  employeeRepository.save(employee);
+        return ResponseEntity.ok(updatedEmployee);
     }
 
 }
